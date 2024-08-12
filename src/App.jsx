eef1161857e7263f9b623766e3ce219c6326b850
nc-naylor <natasha.c.naylor@gmail.com>
2024-08-12 14:04:36 -0700

    Initial file structure commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,8 @@
+export default function TodoInput() {
+    return (
+        <div>
+            TodoInput
+        </div>
+    )
+}
+
